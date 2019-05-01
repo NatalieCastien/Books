3dd71774ab389e65b87d21c910f7c8a56b63f5e8
natalie <natalie.castien@gmail.com>
2019-05-01 17:26:20 +0200

    javascript calculator conceptversie, calculator met forms werkt nog niet

--- js/app.js
+++ js/app.js
@@ -11,12 +11,43 @@ document.addEventListener('DOMContentLoaded',function () {
     document.getElementById('berichtContainer').textContent = tekst;
 
 
-    var getal1 = prompt();
-    var getal2 = prompt();
 
-    // Creer een functie die getallen 1 en 2 bij elkaar optelt. En schrijf de uitkomst weg op scherm.
 
-    // Uitdaging 2 (bij voldoende tijd): Creer een calculator met form veldjes op scherm, als volgt:
-    // INPUT TYPE TEXT (getal 1) - SELECT (met +, -, / , *) - INPUT TYPE TEXT (getal 2) - KNOP ( = ) - INPUT TYPE TEKST (uitkomst)
+    function berekenUitkomst() {
+        var getal1 = prompt('Eerste getal');
+        var getal2 = prompt('tweede getal');
+
+        var uitkomst = parseInt(getal1) + parseInt(getal2);
+        return uitkomst;
+
+    }
+    var result = berekenUitkomst();
+    document.getElementById('rekenUitkomst').textContent = result;
+
+
+
 });
 
+document.getElementById('calc').addEventListener("click", function() {
+    function calculate() {
+        var getal1 = document.getElementById(getal1);
+        var getal2 = document.getElementById(getal2);
+
+        var result = parseInt(getal1) + parseInt(getal2);
+        return result;
+    }
+
+    var result = calculate();
+    document.getElementById('result').textContent = result;
+});
+
+
+
+
+
+
+//     // Creer een functie die getallen 1 en 2 bij elkaar optelt. En schrijf de uitkomst weg op scherm.
+//
+//     // Uitdaging 2 (bij voldoende tijd): Creer een calculator met form veldjes op scherm, als volgt:
+//     // INPUT TYPE TEXT (getal 1) - SELECT (met +, -, / , *) - INPUT TYPE TEXT (getal 2) - KNOP ( = ) - INPUT TYPE TEKST (uitkomst)
+// }
