82f20e070a3e3a7c9c58b34d0ca0af035781e385
natalie.castien@gmail.com <Gitaarsnaar!91>
2019-05-08 21:37:29 +0200

    javascript book-details pagina. Alleen image werkt nog niet.

--- js/controller/book.js
+++ js/controller/book.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded',function () {
     const overviewElement = document.querySelector('.book-overview');
-    const book = getBookByIndex(1);
+    const book = getBookByIndex(2);
 
     document.title = book.title;
     fillBookOverview(overviewElement, book);
--- js/view/book-overview.js
+++ js/view/book-overview.js
@@ -1,4 +1,17 @@
 const fillBookOverview = (overviewElement, book) => {
     console.log(overviewElement, book);
     overviewElement.querySelector('h1').textContent = book.title;
+    overviewElement.querySelector('p.subtitle').textContent = book.subtitle;
+
+    //WERKT NIET
+    // overviewElement.querySelector('img.book-image').src = book.image;
+
+    overviewElement.querySelector('p.isbn').textContent = book.isbn;
+    overviewElement.querySelector('p.author').textContent = book.author;
+    overviewElement.querySelector('p.publisher').textContent = book.publisher;
+    overviewElement.querySelector('p.pages').textContent = book.pages;
+    overviewElement.querySelector('p.price').textContent = book.price;
+
+    overviewElement.querySelector('p.description').textContent = book.description;
+
 };
